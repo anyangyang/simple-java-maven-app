897aca9c08929a2cdcb5212383d1b57de2529315
anyang <anyang_zhu@126.com>
2021-01-10 14:40:22 +0800

    test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! My name is anyang";
+    private final String message = "Hello World! My name is anyang!";
 
     public App() {}
 
